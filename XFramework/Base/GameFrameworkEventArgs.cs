30144ac16eb1c3f853219a628ab4ae6f89d60e71
XuYongGuang <806883646@qq.com>
2017-12-15 01:13:47 +0800

    添加游戏模块处理，框架入口，以及log和引用池的抽象层

---
+++ XFramework/Base/GameFrameworkEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XFramework.Base
+{
+    /// <summary>
+    /// 游戏框架中包含事件数据的类的基类。
+    /// </summary>
+    public abstract class GameFrameworkEventArgs : EventArgs
+        {
+            /// <summary>
+            /// 初始化游戏框架中包含事件数据的类的新实例。
+            /// </summary>
+            public GameFrameworkEventArgs()
+            {
+
+            }
+        }
+    
+}
