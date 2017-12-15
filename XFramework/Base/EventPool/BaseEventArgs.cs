3aa9305594cd5a096e8a128555e3f2170d89da10
XuYongGuang <806883646@qq.com>
2017-12-16 02:25:37 +0800

    增加事件池以，任务池，以及变量的抽象接口

---
+++ XFramework/Base/EventPool/BaseEventArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace XFramework.Base
+{
+    /// <summary>
+    /// 事件基类。
+    /// </summary>
+    public abstract class BaseEventArgs : GameFrameworkEventArgs, IReference
+    {
+        /// <summary>
+        /// 获取类型编号。
+        /// </summary>
+        public abstract int Id
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 清理引用。
+        /// </summary>
+        public abstract void Clear();
+    }
+}
