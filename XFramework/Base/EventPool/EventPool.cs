6e76c9b73173ca4fc372b5f3b535508ab2709971
XuYongGuang <806883646@qq.com>
2017-12-16 14:12:16 +0800

    增加事件管理器和调试窗口管理

--- XFramework/Base/EventPool/EventPool.cs
+++ XFramework/Base/EventPool/EventPool.cs
@@ -185,6 +185,28 @@ namespace XFramework.Base
             }
         }
 
+        /// <summary>
+        /// 抛出事件，这个操作是线程安全的，即使不在主线程中抛出，也可保证在主线程中回调事件处理函数，但事件会在抛出后的下一帧分发。
+        /// </summary>
+        /// <param name="sender">事件源。</param>
+        /// <param name="e">事件参数。</param>
+        public void Fire(object sender, T e)
+        {
+            Event eventNode = new Event(sender, e);
+            lock (m_Events)
+            {
+                m_Events.Enqueue(eventNode);
+            }
+        }
 
+        /// <summary>
+        /// 抛出事件立即模式，这个操作不是线程安全的，事件会立刻分发。
+        /// </summary>
+        /// <param name="sender">事件源。</param>
+        /// <param name="e">事件参数。</param>
+        public void FireNow(object sender, T e)
+        {
+            HandleEvent(sender, e);
+        }
     }
 }
---
+++ XFramework/Event/GameEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using XFramework.Base;
+
+namespace XFramework.Event
+{
+    /// <summary>
+    /// 游戏逻辑事件基类。
+    /// </summary>
+    public abstract class GameEventArgs : BaseEventArgs
+    {
+
+    }
+
+}
