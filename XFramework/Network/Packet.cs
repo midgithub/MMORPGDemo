3271f322dfe4e9080bfd540cd3e74e55f5ba9012
XuYongGuang <806883646@qq.com>
2018-01-04 00:24:01 +0800

    增加网络模块，本地化模块，配置模块

---
+++ XFramework/Network/Packet.cs
@@ -0,0 +1,12 @@
+﻿using XFramework.Base;
+
+namespace XFramework.Network
+{
+    /// <summary>
+    /// 网络消息包基类。
+    /// </summary>
+    public abstract class Packet : BaseEventArgs
+    {
+
+    }
+}
