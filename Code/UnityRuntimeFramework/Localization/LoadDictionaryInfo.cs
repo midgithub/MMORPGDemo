c8584a148e1821364d4cea55b18482043fb2e428
XuYongGuang <806883646@qq.com>
2018-01-27 12:55:36 +0800

    添加声音，本地化模块

---
+++ Code/UnityRuntimeFramework/Localization/LoadDictionaryInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace UnityGameFramework.Runtime
+{
+    internal sealed class LoadDictionaryInfo
+    {
+        private readonly string m_DictionaryName;
+        private readonly object m_UserData;
+
+        public LoadDictionaryInfo(string dictionaryName, object userData)
+        {
+            m_DictionaryName = dictionaryName;
+            m_UserData = userData;
+        }
+
+        public string DictionaryName
+        {
+            get
+            {
+                return m_DictionaryName;
+            }
+        }
+
+        public object UserData
+        {
+            get
+            {
+                return m_UserData;
+            }
+        }
+    }
+}
---
+++ Code/UnityRuntimeFramework/Sound/DefaultSoundGroupHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 默认声音组辅助器。
+    /// </summary>
+    public class DefaultSoundGroupHelper : SoundGroupHelperBase
+    {
+
+    }
+}
