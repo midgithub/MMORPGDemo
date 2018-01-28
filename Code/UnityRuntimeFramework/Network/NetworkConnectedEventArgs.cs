1bdc4085aa0566fcbd389b8a382127e918f45d6d
XuYongGuang <806883646@qq.com>
2018-01-28 12:38:24 +0800

    添加网络和webRequest

---
+++ Code/UnityRuntimeFramework/Network/NetworkConnectedEventArgs.cs
@@ -0,0 +1,67 @@
+﻿using XFramework.Event;
+using XFramework.Network;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// 网络连接成功事件。
+    /// </summary>
+    public sealed class NetworkConnectedEventArgs : GameEventArgs
+    {
+        /// <summary>
+        /// 连接成功事件编号。
+        /// </summary>
+        public static readonly int EventId = typeof(NetworkConnectedEventArgs).GetHashCode();
+
+        /// <summary>
+        /// 获取连接成功事件编号。
+        /// </summary>
+        public override int Id
+        {
+            get
+            {
+                return EventId;
+            }
+        }
+
+        /// <summary>
+        /// 获取网络频道。
+        /// </summary>
+        public INetworkChannel NetworkChannel
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取用户自定义数据。
+        /// </summary>
+        public object UserData
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 清理网络连接成功事件。
+        /// </summary>
+        public override void Clear()
+        {
+            NetworkChannel = default(INetworkChannel);
+            UserData = default(object);
+        }
+
+        /// <summary>
+        /// 填充网络连接成功事件。
+        /// </summary>
+        /// <param name="e">内部事件。</param>
+        /// <returns>网络连接成功事件。</returns>
+        public NetworkConnectedEventArgs Fill(XFramework.Network.NetworkConnectedEventArgs e)
+        {
+            NetworkChannel = e.NetworkChannel;
+            UserData = e.UserData;
+
+            return this;
+        }
+    }
+}
---
+++ Code/UnityRuntimeFramework/WebRequest/WWWFormInfo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    internal sealed class WWWFormInfo
+    {
+        private readonly WWWForm m_WWWForm;
+        private readonly object m_UserData;
+
+        public WWWFormInfo(WWWForm wwwForm, object userData)
+        {
+            m_WWWForm = wwwForm;
+            m_UserData = userData;
+        }
+
+        public WWWForm WWWForm
+        {
+            get
+            {
+                return m_WWWForm;
+            }
+        }
+
+        public object UserData
+        {
+            get
+            {
+                return m_UserData;
+            }
+        }
+    }
+}
