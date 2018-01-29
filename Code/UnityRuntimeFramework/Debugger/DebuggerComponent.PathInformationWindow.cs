41da43a18089e000ddc741c8e6a38826d37b7369
XuYongGuang <806883646@qq.com>
2018-01-29 23:36:52 +0800

    添加Debugger系统

---
+++ Code/UnityRuntimeFramework/Debugger/DebuggerComponent.PathInformationWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public partial class DebuggerComponent
+    {
+        private sealed class PathInformationWindow : ScrollableDebuggerWindowBase
+        {
+            protected override void OnDrawScrollableWindow()
+            {
+                GUILayout.Label("<b>Path Information</b>");
+                GUILayout.BeginVertical("box");
+                {
+                    DrawItem("Data Path:", Application.dataPath);
+                    DrawItem("Persistent Data Path:", Application.persistentDataPath);
+                    DrawItem("Streaming Assets Path:", Application.streamingAssetsPath);
+                    DrawItem("Temporary Cache Path:", Application.temporaryCachePath);
+                }
+                GUILayout.EndVertical();
+            }
+        }
+    }
+}
