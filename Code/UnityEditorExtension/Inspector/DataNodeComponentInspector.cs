c39439c003f4ee1b91a280a6524a1ab6f5934bfc
XuYongGuang <806883646@qq.com>
2018-01-30 11:52:03 +0800

    添加拓展编辑器

---
+++ Code/UnityEditorExtension/Inspector/DataNodeComponentInspector.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEditorExtension.Inspector;
+using UnityGameFramework.Runtime;
+using XFramework.DataNode;
+
+namespace UnityGameFramework.Editor
+{
+    [CustomEditor(typeof(DataNodeComponent))]
+    internal sealed class DataNodeComponentInspector : GameFrameworkInspector
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (!EditorApplication.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Available during runtime only.", MessageType.Info);
+                return;
+            }
+
+            DataNodeComponent t = (DataNodeComponent)target;
+
+            if (PrefabUtility.GetPrefabType(t.gameObject) != PrefabType.Prefab)
+            {
+                DrawDataNode(t.Root);
+            }
+
+            Repaint();
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void DrawDataNode(IDataNode dataNode)
+        {
+            EditorGUILayout.LabelField(dataNode.FullName, dataNode.ToDataString());
+            IDataNode[] child = dataNode.GetAllChild();
+            foreach (IDataNode c in child)
+            {
+                DrawDataNode(c);
+            }
+        }
+    }
+}
---
+++ Code/UnityEditorExtension/Misc/ConfigPathAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityEditorExtension.Misc
+{
+    /// <summary>
+    /// 配置路径属性。
+    /// </summary>
+    public abstract class ConfigPathAttribute : Attribute
+    {
+
+    }
+}
